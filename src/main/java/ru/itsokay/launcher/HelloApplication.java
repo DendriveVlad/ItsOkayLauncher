47622c56a1aae7cb7dee82693295e6804d3fb363
DendriveVlad <DendriveVlad@gmail.com>
2022-05-21 23:30:36 +0500

    update

--- src/main/java/ru/itsokay/launcher/HelloApplication.java
+++ src/main/java/ru/itsokay/launcher/HelloApplication.java
@@ -2,24 +2,17 @@ package ru.itsokay.launcher;
 
 import javafx.application.Application;
 import javafx.concurrent.Worker;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebEvent;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import netscape.javascript.JSObject;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-
-import netscape.javascript.JSObject;
 
 public class HelloApplication extends Application {
     private double xOffset;
@@ -35,8 +28,6 @@ public class HelloApplication extends Application {
         this.stage = stage;
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.setResizable(false);
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         URL url = new File("src/main/resources/ru/itsokay/launcher/test.html").toURI().toURL();
 
         final WebView browser = new WebView();
@@ -45,46 +36,33 @@ public class HelloApplication extends Application {
 
         webEngine.getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
             if (Worker.State.SUCCEEDED == newValue) {
-                // set an interface object named 'javaConnector' in the web engine's page
                 JSObject window = (JSObject) webEngine.executeScript("window");
                 window.setMember("javaConnector", javaConnector);
-
-                // get the Javascript connector object.
                 javascriptConnector = (JSObject) webEngine.executeScript("getJsConnector()");
             }
         });
 
         Scene scene = new Scene(browser, 1280, 720);
 
-        browser.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                if (event.getY() <= 30) {
-                    moveAccess = true;
-                    xOffset = stage.getX() - event.getScreenX();
-                    yOffset = stage.getY() - event.getScreenY();
-                }
+        browser.setOnMousePressed(event -> {
+            if (event.getY() <= 30) {
+                moveAccess = true;
+                xOffset = stage.getX() - event.getScreenX();
+                yOffset = stage.getY() - event.getScreenY();
             }
         });
-        browser.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                if (moveAccess) {
-                    stage.setX(event.getScreenX() + xOffset);
-                    stage.setY(event.getScreenY() + yOffset);
-                }
+        browser.setOnMouseDragged(event -> {
+            if (moveAccess) {
+                stage.setX(event.getScreenX() + xOffset);
+                stage.setY(event.getScreenY() + yOffset);
             }
         });
-        browser.setOnMouseReleased(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                if (moveAccess) {
-                    moveAccess = false;
-                }
+        browser.setOnMouseReleased(mouseEvent -> {
+            if (moveAccess) {
+                moveAccess = false;
             }
         });
 
-//        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("main.css")).toExternalForm());
         browser.setPageFill(Color.TRANSPARENT);
         scene.setFill(Color.TRANSPARENT);
         stage.setTitle("Hello!");
